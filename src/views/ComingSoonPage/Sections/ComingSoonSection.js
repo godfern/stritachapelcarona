9860e02b1788a2434613511c04de2e60df8ce413
Godfrey Fernandes <godfrey.f@ET-C02WK5MEG8WN.local>
2020-04-24 01:26:26 +0530

    Added coming soon page

---
+++ src/views/ComingSoonPage/Sections/ComingSoonSection.js
@@ -0,0 +1,29 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Chat from "@material-ui/icons/Chat";
+import VerifiedUser from "@material-ui/icons/VerifiedUser";
+import Fingerprint from "@material-ui/icons/Fingerprint";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import InfoArea from "components/InfoArea/InfoArea.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ProductSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Coming Soon</h2>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
--- src/views/Components/Sections/HomeSection.js
+++ src/views/Components/Sections/HomeSection.js
@@ -3,19 +3,26 @@ import React from "react";
 import Slider from "nouislider";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
 
 // @material-ui/icons
 
 // core components
 import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
 
 
 import { cardTitle, cardLink, cardSubtitle } from "assets/jss/material-kit-react.js";
 import basicsStyle from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.js";
+import downloadStyle from "assets/jss/material-kit-react/views/componentsSections/downloadStyle.js";
 
 const styles = {
   ...basicsStyle,
+  ...downloadStyle,
   cardTitle,
   cardLink,
   cardSubtitle
@@ -33,49 +40,53 @@ export default function HomeSection() {
   return (
     <div className={classes.sections}>
       <div className={classes.container}>
-      <Card style={{width: "20rem"}}>
-      <CardBody>
-        <h4 className={classes.cardTitle}>Novenas & Feast</h4>
-        <p>
-          Find the latest of the Novenas & Feast details here
-        </p>
-        <a
-          href="#pablo"
-          className={classes.cardLink}
-          onClick={(e) => e.preventDefault()}>
-          View
-        </a>
-      </CardBody>
-    </Card>
-    <Card style={{width: "20rem"}}>
-      <CardBody>
-        <h4 className={classes.cardTitle}>Chapel Announcement</h4>
-        <p>
-          Find the latest from the chapel
-        </p>
-        <a
-          href="#pablo"
-          className={classes.cardLink}
-          onClick={(e) => e.preventDefault()}>
-          View
-        </a>
-      </CardBody>
-    </Card>
-
-    <Card style={{width: "20rem"}}>
-      <CardBody>
-        <h4 className={classes.cardTitle}>Chaplain  Message</h4>
-        <p>
-          Chaplain message to the people of carona
-        </p>
-        <a
-          href="#pablo"
-          className={classes.cardLink}
-          onClick={(e) => e.preventDefault()}>
-          View
-        </a>
-      </CardBody>
-    </Card>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={4}>
+          <Card style={{width: "20rem"}}>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Novenas & Feast</h4>
+              <p>
+                Find the latest of the Novenas & Feast details here
+              </p>
+              <Link to={"/coming-soon-page"} className={classes.link}>
+                  <Button color="primary" size="lg" simple>
+                    View
+                  </Button>
+                </Link>
+            </CardBody>
+          </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Card style={{width: "20rem"}}>
+              <CardBody>
+                <h4 className={classes.cardTitle}>Chapel Announcement</h4>
+                <p>
+                  Find the latest from the Chapel and its future program
+                </p>
+                <Link to={"/coming-soon-page"} className={classes.link}>
+                  <Button color="primary" size="lg" simple>
+                    View
+                  </Button>
+                </Link>
+              </CardBody>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Card style={{width: "20rem"}}>
+              <CardBody>
+                <h4 className={classes.cardTitle}>Chaplain  Message</h4>
+                <p>
+                  Chaplain message to the people of carona
+                </p>
+                <Link to={"/coming-soon-page"} className={classes.link}>
+                  <Button color="primary" size="lg" simple>
+                    View
+                  </Button>
+                </Link>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
       </div>
     </div>
   );
